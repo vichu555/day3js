53cbfea7c690a99974b1c54df4c066e0692b994b
vichu555 <80614368+vichu555@users.noreply.github.com>
2024-04-29 10:45:09 +0530

    Add files via upload

---
+++ day3.js
@@ -0,0 +1,24 @@
+//for,while,do-while
+//for loop
+/*
+let v=5;
+for(let i=1;i<=10;i++){
+    console.log(v +" * "+ i + " = " + v*i)
+}
+*/
+//while loop
+/*
+let s=2;
+let i=1;
+while(i <= 10){
+console.log(s +" * "+ i + " = " + s*i);
+i++;
+}
+*/
+//do-while loop
+let a=3;
+let i=1;
+do{
+    console.log(a +" * "+ i + " = " + a*i);
+     i++;
+}while(i <= 10)
